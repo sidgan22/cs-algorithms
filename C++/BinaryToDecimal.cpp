3fb0cf020be68b365aed5275e78ffd86d24cd5d7
sidgan22 <sidgan22@gmail.com>
2019-10-14 21:53:03 +0530

    Update BinaryToDecimal.cpp
    Using a string gives room for larger numbers (in binary)

--- C++/BinaryToDecimal.cpp
+++ C++/BinaryToDecimal.cpp
@@ -2,20 +2,14 @@
 using namespace std;
 int main()
 {
-	int n;
-	cin>>n;
-	int temp=n;
-	int dec=0;
-	int t=1;
-	int base=1;
-	while(temp)
-	{
-		t=temp%10;
-		dec=dec+t*base;
-		base=base*2;
-		temp=temp/10;
-
-	}
+	string num = n; 
+    	int dec = 0,base = 1, len = num.length(); 
+    	for (int i = len - 1; i >= 0; i--) 
+	{ 
+            if (num[i] == '1') 
+                dec += base; 
+            base = base * 2; 
+    	} 
 	cout<<dec<<endl;
 	return 0;
 }
